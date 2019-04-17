e392df0513cdd6dd2fa626930026c1662668a04b
domkedz <1domkedz1@gmail.com>
2019-04-17 18:15:20 +0200

    settings update

--- django/weird/weird/settings.py
+++ django/weird/weird/settings.py
@@ -81,16 +81,17 @@ WSGI_APPLICATION = 'weird.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS'],
-        'HOST': os.environ['DB_SERVICE'],
-        'PORT': os.environ['DB_PORT']
+if not os.environ['ENVIRONMENT'] == 'PROD':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASS'],
+            'HOST': os.environ['DB_SERVICE'],
+            'PORT': os.environ['DB_PORT']
+        }
     }
-}
 
 
 # Password validation
